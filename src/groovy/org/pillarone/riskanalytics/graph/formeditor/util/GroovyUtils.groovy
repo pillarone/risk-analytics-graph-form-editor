a53dfd53bd4b81175cf28deecf5c46c7e9f2b71c
Martin Melchior <martin.melchior@fhnw.ch>
2011-05-10 16:31:44 +0200

    Adding obtainPorts-method

--- src/groovy/org/pillarone/riskanalytics/graph/formeditor/util/GroovyUtils.groovy
+++ src/groovy/org/pillarone/riskanalytics/graph/formeditor/util/GroovyUtils.groovy
@@ -2,6 +2,13 @@ package org.pillarone.riskanalytics.graph.formeditor.util
 
 import org.pillarone.riskanalytics.graph.core.loader.ClassRepository
 import org.pillarone.riskanalytics.graph.core.loader.DatabaseClassLoader
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
+import org.pillarone.riskanalytics.core.packets.Packet
+import org.pillarone.riskanalytics.core.packets.PacketList
+import java.lang.reflect.Field
+import org.pillarone.riskanalytics.core.components.Component
+import org.pillarone.riskanalytics.graph.core.palette.model.ComponentDefinition
 
 class GroovyUtils {
 
@@ -14,5 +21,26 @@ class GroovyUtils {
             classLoader.refresh()
         }
         return classLoader.loadClass(name)
+    }
+
+    public static Map<Field, Class> obtainPorts(ComponentDefinition definition, String prefix) {
+        Map<String, Class> result = [:]
+
+        Class currentClass = definition.typeClass
+        while (currentClass != Component.class) {
+            for (Field field in currentClass.declaredFields) {
+                if (field.name.startsWith(prefix) && PacketList.isAssignableFrom(field.type)) {
+                    Class packetType = Packet
+                    Type genericType = field.genericType
+                    if (genericType instanceof ParameterizedType) {
+                        packetType = genericType.actualTypeArguments[0]
+                    }
+                    result.put(field, packetType)
+                }
+            }
+            currentClass = currentClass.superclass
+        }
+
+        return result
     }
 }
