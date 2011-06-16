0d04de6940c0f8b6310395e28385b22d2dc74881
Martin Melchior <martin.melchior@fhnw.ch>
2011-06-16 18:09:26 +0200

    adding getValuesToSave-method

--- src/groovy/org/pillarone/riskanalytics/graph/formeditor/examples/ClaimPacket.groovy
+++ src/groovy/org/pillarone/riskanalytics/graph/formeditor/examples/ClaimPacket.groovy
@@ -3,12 +3,15 @@ package org.pillarone.riskanalytics.graph.formeditor.examples
 import org.pillarone.riskanalytics.core.packets.Packet
 
 /**
- * Created by IntelliJ IDEA.
- * User: martin.melchior
- * Date: 06.06.11
- * Time: 15:30
- * To change this template use File | Settings | File Templates.
+ * 
  */
 class ClaimPacket extends Packet {
+
     double value
+
+    public Map<String, Number> getValuesToSave() throws IllegalAccessException {
+        Map<String, Number> valuesToSave = new HashMap<String, Number>(1);
+        valuesToSave.put("claim", value);
+        return valuesToSave;
+    }
 }
--- src/groovy/org/pillarone/riskanalytics/graph/formeditor/examples/FrequencyPacket.groovy
+++ src/groovy/org/pillarone/riskanalytics/graph/formeditor/examples/FrequencyPacket.groovy
@@ -6,5 +6,12 @@ import org.pillarone.riskanalytics.core.packets.Packet
  * 
  */
 class FrequencyPacket extends Packet {
+
     int value
+
+    public Map<String, Number> getValuesToSave() throws IllegalAccessException {
+        Map<String, Number> valuesToSave = new HashMap<String, Number>(1);
+        valuesToSave.put("frequency", value);
+        return valuesToSave;
+    }
 }
