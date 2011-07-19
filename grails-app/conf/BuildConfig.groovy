efe0c1e2bffa8bfc5c397408531b944c2cb0f5f3
Martin Melchior <martin.melchior@fhnw.ch>
2011-07-20 00:46:51 +0200

    graph core inplace

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -44,8 +44,8 @@ grails.project.dependency.resolution = {
         test ":code-coverage:1.2.2"
 
         if (appName == "RiskAnalyticsGraphFormEditor") {
-            runtime "org.pillarone:risk-analytics-core:1.4-ALPHA-2.5.1-kti"
-            runtime("org.pillarone:risk-analytics-graph-core:0.3.3") { transitive = false }
+            runtime "org.pillarone:risk-analytics-core:1.4-ALPHA-2.5.5-kti"
+            // runtime("org.pillarone:risk-analytics-graph-core:0.3.3") { transitive = false }
         }
 
     }
@@ -94,5 +94,9 @@ coverage {
             '**GrailsPlugin**',
             '**TagLib**'
     ]
+
+    grails.plugin.location.'risk-analytics-graph-core' = "../RiskAnalyticsGraphCore"
+    /*grails.plugin.location.'risk-analytics-pc' = "../RiskAnalyticsCore"
+    grails.plugin.location.'risk-analytics-pc' = "../RiskAnalyticsPropertyCasualty"*/
 
 }
