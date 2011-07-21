057faae942929dbc7c940c30a8d43583195d8e62
Martin Melchior <martin.melchior@fhnw.ch>
2011-07-22 00:28:01 +0200

    Context menu with expand / collapse

--- src/groovy/org/pillarone/riskanalytics/graph/formeditor/ui/view/SimulationResultTable.java
+++ src/groovy/org/pillarone/riskanalytics/graph/formeditor/ui/view/SimulationResultTable.java
@@ -1,7 +1,12 @@
 package org.pillarone.riskanalytics.graph.formeditor.ui.view;
 
 import com.ulcjava.base.application.ClientContext;
+import com.ulcjava.base.application.ULCMenuItem;
+import com.ulcjava.base.application.ULCPopupMenu;
 import com.ulcjava.base.application.ULCTableTree;
+import com.ulcjava.base.application.event.ActionEvent;
+import com.ulcjava.base.application.event.IActionListener;
+import com.ulcjava.base.application.tree.TreePath;
 import com.ulcjava.base.application.util.Dimension;
 import org.pillarone.riskanalytics.core.simulation.item.Parameterization;
 import org.pillarone.riskanalytics.graph.core.graph.model.ModelGraphModel;
@@ -17,11 +22,15 @@ public class SimulationResultTable extends ULCTableTree {
 
     SimulationResultDataTreeModel fTableModel;
 
-
     public SimulationResultTable(Map simulationOutput) {
+        createView();
+        setData(simulationOutput);
+    }
+
+    public void setData(Map simulationOutput) {
         fTableModel = new SimulationResultDataTreeModel(simulationOutput);
         this.setModel(fTableModel);
-        createView();
+        this.expandAll();
     }
 
     public void createView() {
@@ -32,5 +41,44 @@ public class SimulationResultTable extends ULCTableTree {
         int preferredWidth = width / 2;
         int preferredHeight = preferredWidth * height * 10 / (width * 11 * 4);
         this.setPreferredScrollableViewportSize(new Dimension(preferredWidth, preferredHeight));
+        createContextMenu();
     }
+
+    private void createContextMenu() {
+        ULCPopupMenu menu = new ULCPopupMenu();
+
+        ULCMenuItem expandItem = new ULCMenuItem("expand");
+        expandItem.addActionListener(new IActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                TreePath[] selectedPaths = SimulationResultTable.this.getSelectedPaths();
+                SimulationResultTable.this.expandPaths(selectedPaths, true);
+            }
+        });
+        menu.add(expandItem);
+        ULCMenuItem expandAllItem = new ULCMenuItem("expand all");
+        expandAllItem.addActionListener(new IActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                SimulationResultTable.this.expandAll();
+            }
+        });
+        menu.add(expandAllItem);
+        ULCMenuItem collapseItem = new ULCMenuItem("collapse");
+        collapseItem.addActionListener(new IActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                TreePath[] selectedPaths = SimulationResultTable.this.getSelectedPaths();
+                SimulationResultTable.this.collapsePaths(selectedPaths, true);
+            }
+        });
+        menu.add(collapseItem);
+        ULCMenuItem collapseAllItem = new ULCMenuItem("collapse all");
+        collapseAllItem.addActionListener(new IActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                SimulationResultTable.this.collapseAll();
+            }
+        });
+        menu.add(collapseAllItem);
+
+        this.setComponentPopupMenu(menu);
+    }
+
 }
