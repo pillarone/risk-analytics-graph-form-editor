844805345de565859526f436b17cb1ca31b44c1f
Martin Melchior <martin.melchior@fhnw.ch>
2011-09-01 13:06:54 +0200

    New examples components

---
+++ src/groovy/org/pillarone/riskanalytics/graph/formeditor/examples/LoB.groovy
@@ -0,0 +1,59 @@
+package org.pillarone.riskanalytics.graph.formeditor.examples;
+
+
+import org.pillarone.riskanalytics.core.components.ComponentCategory
+import org.pillarone.riskanalytics.core.components.ComposedComponent
+import org.pillarone.riskanalytics.core.packets.PacketList
+import org.pillarone.riskanalytics.core.wiring.PortReplicatorCategory
+import org.pillarone.riskanalytics.core.wiring.WireCategory
+import org.pillarone.riskanalytics.core.wiring.WiringUtils
+
+/**
+ * 
+ */
+@ComponentCategory(categories=["Claims","Risk"])
+public class LoB extends ComposedComponent {
+    PacketList<ClaimPacket> outSingleGrossClaims = new PacketList<ClaimPacket>(ClaimPacket.class);
+    PacketList<ClaimPacket> outSingleRetainedClaims = new PacketList<ClaimPacket>(ClaimPacket.class);
+    PacketList<ClaimPacket> outAttrGrossClaims = new PacketList<ClaimPacket>(ClaimPacket.class);
+    PacketList<ClaimPacket> outAttrRetainedClaims = new PacketList<ClaimPacket>(ClaimPacket.class);
+    PacketList<ClaimPacket> outTotalGrossClaims = new PacketList<ClaimPacket>(ClaimPacket.class);
+    PacketList<ClaimPacket> outTotalNetClaims = new PacketList<ClaimPacket>(ClaimPacket.class);
+
+    PoissonFrequencyGenerator subLargeFrequencyGen = new PoissonFrequencyGenerator(name: 'subLargeFrequencyGen');
+    SingleLogNormalClaimsGenerator subLargeClaimsGen = new SingleLogNormalClaimsGenerator(name: 'subLargeClaimsGen');
+    AggregateNormalClaimGenerator subAttrClaimsGen = new AggregateNormalClaimGenerator(name: 'subAttrClaimsGen');
+
+    QuotaShare subQuotaShareContract = new QuotaShare(name: 'subQuotaShareContract');
+    ExcessOfLoss subXLContract = new ExcessOfLoss(name: 'subXLContract');
+
+    Aggregator subAggregatorGross = new Aggregator(name: 'subAggregatorGross');
+    Aggregator subAggregatorNet = new Aggregator(name: 'subAggregatorNet');
+
+    @Override
+    public void wire() {
+        WiringUtils.use(PortReplicatorCategory) {
+            this.outSingleGrossClaims = subLargeClaimsGen.outClaims;
+            this.outSingleRetainedClaims = subXLContract.outRetainedClaims;
+            this.outAttrGrossClaims = subAttrClaimsGen.outClaims;
+            this.outAttrRetainedClaims = subQuotaShareContract.outRetainedAggregateClaims;
+            this.outTotalGrossClaims = subAggregatorGross.outClaims;
+            this.outTotalNetClaims = subAggregatorNet.outClaims;
+        }
+
+        WiringUtils.use(WireCategory) {
+            subLargeClaimsGen.inFrequency = subLargeFrequencyGen.outFrequency
+
+            subQuotaShareContract.inAggregateClaims = subAttrClaimsGen.outClaims
+            subQuotaShareContract.inSingleClaims = subLargeClaimsGen.outClaims
+
+            subXLContract.inClaims = subQuotaShareContract.outRetainedSingleClaims
+
+            subAggregatorGross.inClaims = subLargeClaimsGen.outClaims
+            subAggregatorGross.inClaims = subAttrClaimsGen.outClaims
+
+            subAggregatorNet.inClaims = subXLContract.outRetainedClaims
+            subAggregatorNet.inClaims = subQuotaShareContract.outRetainedAggregateClaims
+        }
+    }
+}
---
+++ src/groovy/org/pillarone/riskanalytics/graph/formeditor/examples/QuotaShare.java
@@ -0,0 +1,105 @@
+package org.pillarone.riskanalytics.graph.formeditor.examples;
+
+import org.pillarone.riskanalytics.core.components.Component;
+import org.pillarone.riskanalytics.core.components.ComponentCategory;
+import org.pillarone.riskanalytics.core.packets.PacketList;
+import org.pillarone.riskanalytics.core.wiring.WiringValidation;
+
+/**
+ * 
+ */
+@ComponentCategory(categories={"Claims","Reinsurance","Risk"})
+public class QuotaShare extends Component {
+
+    private double parmQuota = 0.0;
+
+    private PacketList<ClaimPacket> inSingleClaims = new PacketList<ClaimPacket>(ClaimPacket.class);
+    private PacketList<ClaimPacket> inAggregateClaims = new PacketList<ClaimPacket>(ClaimPacket.class);
+
+    private PacketList<ClaimPacket> outCededSingleClaims = new PacketList<ClaimPacket>(ClaimPacket.class);
+    private PacketList<ClaimPacket> outCededAggregateClaims = new PacketList<ClaimPacket>(ClaimPacket.class);
+    private PacketList<ClaimPacket> outRetainedSingleClaims = new PacketList<ClaimPacket>(ClaimPacket.class);
+    private PacketList<ClaimPacket> outRetainedAggregateClaims = new PacketList<ClaimPacket>(ClaimPacket.class);
+
+    public void doCalculation() {
+        for (ClaimPacket claim : getInAggregateClaims()) {
+            double gross = claim.getValue();
+            double ceded = getParmQuota()*gross;
+            double retained = gross-ceded;
+            ClaimPacket claimCeded = new ClaimPacket();
+            claimCeded.setValue(ceded);
+            getOutCededAggregateClaims().add(claimCeded);
+            ClaimPacket claimRetained = new ClaimPacket();
+            claimRetained.setValue(retained);
+            getOutRetainedAggregateClaims().add(claimRetained);
+        }
+
+        for (ClaimPacket claim : getInSingleClaims()) {
+            double gross = claim.getValue();
+            double ceded = getParmQuota()*gross;
+            double retained = gross-ceded;
+            ClaimPacket claimCeded = new ClaimPacket();
+            claimCeded.setValue(ceded);
+            getOutCededSingleClaims().add(claimCeded);
+            ClaimPacket claimRetained = new ClaimPacket();
+            claimRetained.setValue(retained);
+            getOutRetainedSingleClaims().add(claimRetained);
+        }
+    }
+
+    public double getParmQuota() {
+        return parmQuota;
+    }
+
+    public void setParmQuota(double qs) {
+        this.parmQuota = qs;
+    }
+
+    public PacketList<ClaimPacket> getInSingleClaims() {
+        return inSingleClaims;
+    }
+
+    public void setInSingleClaims(PacketList<ClaimPacket> inSingleClaims) {
+        this.inSingleClaims = inSingleClaims;
+    }
+
+    public PacketList<ClaimPacket> getInAggregateClaims() {
+        return inAggregateClaims;
+    }
+
+    public void setInAggregateClaims(PacketList<ClaimPacket> inAggregateClaims) {
+        this.inAggregateClaims = inAggregateClaims;
+    }
+
+    public PacketList<ClaimPacket> getOutCededSingleClaims() {
+        return outCededSingleClaims;
+    }
+
+    public void setOutCededSingleClaims(PacketList<ClaimPacket> outCededSingleClaims) {
+        this.outCededSingleClaims = outCededSingleClaims;
+    }
+
+    public PacketList<ClaimPacket> getOutCededAggregateClaims() {
+        return outCededAggregateClaims;
+    }
+
+    public void setOutCededAggregateClaims(PacketList<ClaimPacket> outCededAggregateClaims) {
+        this.outCededAggregateClaims = outCededAggregateClaims;
+    }
+
+    public PacketList<ClaimPacket> getOutRetainedSingleClaims() {
+        return outRetainedSingleClaims;
+    }
+
+    public void setOutRetainedSingleClaims(PacketList<ClaimPacket> outRetainedSingleClaims) {
+        this.outRetainedSingleClaims = outRetainedSingleClaims;
+    }
+
+    public PacketList<ClaimPacket> getOutRetainedAggregateClaims() {
+        return outRetainedAggregateClaims;
+    }
+
+    public void setOutRetainedAggregateClaims(PacketList<ClaimPacket> outRetainedAggregateClaims) {
+        this.outRetainedAggregateClaims = outRetainedAggregateClaims;
+    }
+}
